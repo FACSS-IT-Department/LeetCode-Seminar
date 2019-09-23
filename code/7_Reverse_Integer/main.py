33bcdb13e8d41bbd868144330a479e967d9d4b6f
Yan <44847194+Cookies-Yan@users.noreply.github.com>
2019-09-22 21:19:50 -0500

    Update main.py

--- code/7_Reverse_Integer/main.py
+++ code/7_Reverse_Integer/main.py
@@ -27,3 +27,41 @@ if __name__ == '__main__':
     print(Solution.reverse(2 ** 31 - 1))
     print(Solution.reverse(-(2 ** 31)))
     print(Solution.reverse(-8463847412))
+
+############################ 
+#########In JAVA
+############################ 
+package leetcode;
+
+/**
+ * Created by Cookies
+ *
+ * Reverse Interger
+ * Reverse digits of an integer
+ *
+ * Example: x = 123, return 321
+ * Example: x = -123, return -321
+ */
+
+
+public class Reverse_Integer_07 {
+ /** int:
+  * -2^31 ~ 2^31 - 1 (-2147483648 ~ 2147483647)
+  *
+  * Corner case : 越界
+  *
+  * Time: O(n)
+  * Space: O(1)
+  *
+  */
+    public static int reverse(int x){
+         long res = 0;
+         while(x != 0){
+             res = res * 10 + x % 10;
+             x /= 10;
+             if(res > Integer.MAX_VALUE || res < Integer.MIN_VALUE) return 0;
+         }
+         return (int)res;
+    }
+}
+    
