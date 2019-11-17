ad510569a3e0516423c8859befe1230dfd09282d
Dayuan Chen <neemo.cdy@gmail.com>
2019-11-17 16:21:25 -0600

    Update 283.cpp

--- code/283. Move Zeros/283.cpp
+++ code/283. Move Zeros/283.cpp
@@ -5,6 +5,8 @@ public:
     void moveZeroes(vector<int>& nums) {
         int i;
         int j;
+        
+        // method 1
         for (i = 0; i < nums.size(); i++)
         {
             if(nums[i] != 0)
@@ -18,5 +20,17 @@ public:
             nums[j] = 0;
             j++;
         }
+        
+        // method 2
+        /*  // since we won't use i'th number in the array, we can just put that place to "0"
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
+        }
+        */
     }
 };
